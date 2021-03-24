7951bc7e91a7db290f32c753292404e9928fc31f
JSBHernandez <80870910+JSBHernandez@users.noreply.github.com>
2021-03-24 10:44:27 -0500

    Laboratorio de Bucles Anidados
    24 de febrero de 2021

---
+++ Lab_2303/CicloAnd1.py
@@ -0,0 +1,3 @@
+for x in range (5):
+    for z in range(x):
+        print ("x vale: ",x," y z vale: ", z)
---
+++ Lab_2303/CicloAnd2.py
@@ -0,0 +1,3 @@
+for x in range (5):
+    for z in range(4):
+        print ("x vale: ",x," y z vale: ", z)
