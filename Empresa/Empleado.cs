2c48b46b63fde69bc30edf4a3f3c876b3cbe05dc
Juan Becerra <80870910+JSBHernandez@users.noreply.github.com>
2021-11-11 19:03:20 -0500

    Company

---
+++ Empresa/Empleado.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa
+{
+    class Empleado
+    {
+        //Atributos
+
+        String nombre;
+        String apellido;
+        int DNI;
+        String direccion;
+        int telefono;
+        double salario;
+        int antiguedad;
+        Empleado supervisor;
+
+        // COnstructor
+
+        public Empleado(String nombre, String apellido, int DNI, String direccion, int telefono, double salario, int antiguedad, Empleado supervisor)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.DNI = DNI;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.salario = salario;
+            this.antiguedad = antiguedad;
+            this.supervisor = supervisor;
+        }
+
+        public void Imprimir()
+        {
+            Console.Write("nombre" + nombre);
+            Console.WriteLine(nombre);
+            Console.Write("apellido" + apellido);
+            Console.WriteLine(apellido);
+            Console.Write("DNI" + DNI);
+            Console.WriteLine(DNI);
+            Console.Write("direccion" + direccion);
+            Console.WriteLine(direccion);
+            Console.Write("telefono" + telefono);
+            Console.WriteLine(telefono);
+            Console.Write("salario" + salario);
+            Console.WriteLine(salario);
+            Console.Write("antiguedad" + antiguedad);
+            Console.WriteLine(antiguedad);    
+        }
+
+        public void cambiar (Empleado nuevosupervisor)
+        {
+            supervisor = nuevosupervisor;
+        }
+
+        public void incrementar (double incremento)
+        {
+            salario = salario + incremento * salario;
+            Console.Write("El sueldo del empleado con el aumento es de" + salario);
+            Console.WriteLine(salario);
+        }
+    }
+}
---
+++ Empresa/Jefe de Zona.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa
+{
+    class Jefe_de_Zona : Empleado
+    {
+        //Atributos
+
+        String coche;
+
+        public Jefe_de_Zona(String coche, String nombre, String apellido, int DNI, String direccion, int telefono, double salario, int antiguedad, Empleado supervisor) : 
+            base(nombre, apellido, DNI, direccion, telefono, salario, antiguedad, supervisor)
+        {
+            this.coche = coche;
+        }
+
+        Array List<Vendedores> (vendedores= new ArrayList<>());
+        Array List<Secretario> (secretarios= new ArrayList<>());
+        Array List<Vendedores> (vendedores= new ArrayList<>());
+
+        public void asignarVendedor(Vendedor nuevoVendedor)
+        {
+            this.vendedores.add(nuevoVendedor);
+            nuevoVendedor.cambiar(this);
+        }
+
+        public void mostrarVendedor()
+        {
+            for(int i=0; i < this.vendedores.size(); i++)
+            {
+                this.secretrario.get(i).imprimir();
+            }
+        }
+
+        public void DaraltaSecretario(Empleado nuevoSecretario)
+        {
+            this.supervisor = nuevoSecretario;
+            Console.Write("dar de alta " + nuevoSecretario);
+
+        }
+        public void DarbajaSecretario(Empleado nuevoSecretario)
+        {
+            this.supervisor = nuevoSecretario;
+            Console.Write("daeo de baja " + nuevoSecretario);
+
+        }
+
+        public new void Imprimir()
+        {
+            base.Imprimir();
+            Console.Write("El auto asignado es: " + this.coche);
+            Console.WriteLine(coche);
+        }
+    }
+}
